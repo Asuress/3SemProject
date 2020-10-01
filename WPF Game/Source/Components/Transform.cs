8cfd6fc76c216a3280436576afabe91770dab1f9
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-01 17:27:28 +0300

    Added base Components
    added IComponent, Physics, Transform

---
+++ WPF Game/Source/Components/Transform.cs
@@ -0,0 +1,24 @@
+﻿using Platformerengine.res.code.logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Game.Source.Components
+{
+    class Transform : IComponent
+    {
+        public Transform(Point pos, Size size)
+        {
+            Position = pos;
+            Size = size;
+        }
+        public Transform(double x, double y, double width, double height) 
+            : this(new Point(x, y), new Size(width, height))
+        {
+
+        }
+
+        public Point Position { get; set; }
+        public Size Size { get; }
+    }
+}
