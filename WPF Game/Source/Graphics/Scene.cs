341d95e715c0bfc8bb0f02c77c76e75a0dc73cb9
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-01 17:55:13 +0300

    Added Scene class

---
+++ WPF Game/Source/Graphics/Scene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPF_Game.Source.Logic;
+
+namespace WPF_Game.Source.Graphics
+{
+    class Scene
+    {
+        public static Scene GetInstance()
+        {
+            if (Instances == null)
+                Instances = new Scene();
+            return Instances;            
+        }
+        static Scene Instances;
+        Scene()
+        {
+            
+        }
+        List<GameObject> GameObjects;
+    }
+}
