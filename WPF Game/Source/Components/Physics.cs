b4861baed9e0f147658077d18bc23c0e9e3bbc85
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-01 17:27:28 +0300

    Added base Components
    added IComponent, Physics, Transform

---
+++ WPF Game/Source/Components/Physics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Game.Source.Components
+{
+    class Physics : IComponent
+    {
+        public enum Mode
+        {
+            Force,
+            Impulse
+        }
+        Physics()
+        {
+
+        }
+    }
+}
