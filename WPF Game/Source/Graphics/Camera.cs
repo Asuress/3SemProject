ccc7642920f9413a33577d5ecdb2878d0d71f192
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-21 17:21:23 +0300

    Scene

---
+++ WPF Game/Source/Graphics/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using WPF_Game.Source.Logic;
+
+namespace WPF_Game.Source.Graphics
+{
+    class Camera
+    {
+        public Camera(Scene scene, Point position, GameObject parent = null)
+        {
+            MainScene = scene;
+            Position = position;
+            Parent = parent;
+        }
+        public Scene MainScene { get; set; }
+        public Point Position { get; set; }
+        public GameObject Parent { get; private set; }
+    }
+}
--- WPF Game/Source/Graphics/Scene.cs
+++ WPF Game/Source/Graphics/Scene.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Windows.Controls;
 using WPF_Game.Source.Logic;
 
 namespace WPF_Game.Source.Graphics
 {
-    class Scene
+    public class Scene : Canvas
     {
+        public class SceneChangedEventArgs
+        {
+            public SceneChangedEventArgs(LinkedList<GameObject> oldScene, LinkedList<GameObject> newScene)
+            {
+                OldScene = oldScene;
+                NewScene = newScene;
+            }
+            public LinkedList<GameObject> OldScene { get; }
+            public LinkedList<GameObject> NewScene { get; }
+        }
+        public delegate void SceneChangedHandler(object sender, SceneChangedEventArgs e);
+        public event SceneChangedHandler SceneChanged = delegate { };
         public static Scene GetInstance()
         {
             if (Instances == null)
@@ -16,8 +28,21 @@ namespace WPF_Game.Source.Graphics
         static Scene Instances;
         Scene()
         {
-            
+            GameObjects = new LinkedList<GameObject>();
         }
-        List<GameObject> GameObjects;
+        public LinkedList<GameObject> GameObjects { get; }
+        public void AddGameObject(GameObject gameObject)
+        {
+            GameObjects?.AddLast(gameObject ?? throw new Exception("null game object"));
+            Children.Add(gameObject.Shape);
+        }
+        public void RemoveGameObject(GameObject go)
+        {
+            GameObjects.Remove(go);
+        }
+        public bool ContainsGameObject(GameObject gameObject)
+        {
+            return GameObjects.Contains(gameObject);
+        }
     }
 }
