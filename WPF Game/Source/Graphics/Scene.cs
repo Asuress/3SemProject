6c91e8975ac937d9033534f38c2da3f0a756b4e5
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-21 17:23:14 +0300

    changes

---
+++ WPF Game/Source/Graphics/Scene.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using WPF_Game.Source.Logic;
+
+namespace WPF_Game.Source.Graphics
+{
+    public class Scene : Canvas
+    {
+        public class SceneChangedEventArgs
+        {
+            public SceneChangedEventArgs(LinkedList<GameObject> oldScene, LinkedList<GameObject> newScene)
+            {
+                OldScene = oldScene;
+                NewScene = newScene;
+            }
+            public LinkedList<GameObject> OldScene { get; }
+            public LinkedList<GameObject> NewScene { get; }
+        }
+        public delegate void SceneChangedHandler(object sender, SceneChangedEventArgs e);
+        public event SceneChangedHandler SceneChanged = delegate { };
+        public static Scene GetInstance()
+        {
+            if (Instances == null)
+                Instances = new Scene();
+            return Instances;            
+        }
+        static Scene Instances;
+        Scene()
+        {
+            GameObjects = new LinkedList<GameObject>();
+        }
+        public LinkedList<GameObject> GameObjects { get; }
+        public void AddGameObject(GameObject gameObject)
+        {
+            GameObjects?.AddLast(gameObject ?? throw new Exception("null game object"));
+            Children.Add(gameObject.Shape);
+        }
+        public void RemoveGameObject(GameObject go)
+        {
+            GameObjects.Remove(go);
+        }
+        public bool ContainsGameObject(GameObject gameObject)
+        {
+            return GameObjects.Contains(gameObject);
+        }
+    }
+}
---
+++ WPF Game/Source/Logic/CustomScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WPF_Game.Source.Logic;
+
+namespace WPF_Game.source.Logic
+{
+    public class CustomScript
+    {
+        protected void Update()
+        {
+            
+        }
+        protected void LateUpdate()
+        {
+            
+        }
+    }
+}
