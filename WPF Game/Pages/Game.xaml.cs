f23f16718ac0f23b333ae38d86af2c5e5992b9eb
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-25 22:51:21 +0300

    load settings from file

--- WPF Game/Pages/Game.xaml.cs
+++ WPF Game/Pages/Game.xaml.cs
@@ -2,16 +2,15 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using WPF_Game.Source.Main;
 using WPF_Game.Source.Logic;
 using WPF_Game.Source.Graphics;
-using Transform = WPF_Game.Source.Components.Transform;
 using Point = WPF_Game.Source.Logic.Point;
 using Size = WPF_Game.Source.Logic.Size;
 using WPF_Game.Source.Physics;
+using WPF_Game.Source.FileManagers;
 
 namespace WPF_Game.Pages
 {
@@ -36,9 +35,11 @@ namespace WPF_Game.Pages
         Scene _Scene;
         EnemyControl EnemyController;
         GameObject Player;
+        SettingsManager settingsManager = SettingsManager.GetSettingsManager();
         public void Start()
         {
-            ImageBrush playerIMG = new ImageBrush(new BitmapImage(new Uri(@"C:\Users\Admin\source\repos\3SemProject\WPF Game\source\images\Player.png", UriKind.Absolute)));
+            settingsManager.ReadSettings();
+            ImageBrush playerIMG = new ImageBrush(new BitmapImage(new Uri(settingsManager.Settings["PlayerImgPath"], UriKind.Absolute)));
             Shape playerEllipse = new Ellipse();
             Point pt = new Point(Application.Current.MainWindow.ActualWidth / 2, Application.Current.MainWindow.ActualHeight / 2);
             Size sz = new Size(20, 20);
